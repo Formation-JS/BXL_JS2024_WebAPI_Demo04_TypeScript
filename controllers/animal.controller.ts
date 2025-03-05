ba310c89af7665750c3a59be51e8623a5cffba2c
Aurélien St <aurelien.strimelle@bstorm.be>
2025-03-05 16:15:45 +0100

    Pagination middleware 🦊

--- controllers/animal.controller.ts
+++ controllers/animal.controller.ts
@@ -1,4 +1,6 @@
 import type { Controller } from "../@types/common.d.ts";
+import animalData from '../data/animals.json';
+import { RequestWithPagination } from "../middlewares/pagination.middleware.ts";
 
 const animalController = {
 
@@ -6,16 +8,33 @@ const animalController = {
      * RequestHandler qui permet de gérer la route "(GET) /"
      */
     getAll: (req, res) => {
-        res.sendStatus(501);
+        const {offset, limit} = (req as RequestWithPagination).pagination;
+
+        const animals = animalData.slice(offset, offset + limit);
+        res.status(200).json(animals);
     },
 
     /**
      * RequestHandler qui permet de gérer la route "(GET) /:id"
      */
     get: (req, res) => {
-        res.sendStatus(501);
+        const id = parseInt(req.params.id);
+
+        if(isNaN(id)) {
+            res.status(400).json({ error: 'Bad parameter Id !'});
+            return;
+        }
+
+        const animal = animalData.find(a => a.id === id);
+
+        if(!animal) {
+            res.sendStatus(404);
+            return;
+        }
+
+        res.status(200).json(animal);
     }
-    
+
 } as const satisfies Controller;
 
 export default animalController;
---
+++ middlewares/pagination.middleware.ts
@@ -0,0 +1,46 @@
+import type { NextFunction, Request, Response } from "express";
+
+export interface RequestWithPagination extends Request  {
+    pagination: {
+        offset: number;
+        limit: number;
+    };
+};
+
+export function paginationMiddleware({
+    defaultLimit = 10,
+    maxLimit = 50
+} = {}) {
+
+    /**
+     * Middleware de pagination
+     */
+    return function (req: RequestWithPagination, res: Response, next: NextFunction) {
+
+        const offsetRecive = (Array.isArray(req.query.offset) ? req.query.offset[0] : req.query.offset)?.toString();
+        const limitRecive = (Array.isArray(req.query.limit) ? req.query.limit[0] : req.query.limit)?.toString();
+
+        //! Traitement des infos
+        const offset = parseInt(offsetRecive || '0');
+
+        const requestLimit = parseInt(limitRecive || defaultLimit.toString());
+        const limit = Math.min(requestLimit, maxLimit);
+
+        //! Erreur si une des valeurs est négative
+        if (isNaN(offset) || isNaN(limit) || offset < 0 || limit < 0) {
+            res.status(400).json({ error: 'Value of "offset" or "limit" must be positive' });
+            return;
+        }
+
+        //! Injection de données dans l'objet "req"
+        //? On peut également utiliser l'objet "res", en fonction du cas de figure.
+        req.pagination = {
+            offset,
+            limit
+        };
+
+        //! On passe au prochain middleware
+        next();
+    };
+}
+
